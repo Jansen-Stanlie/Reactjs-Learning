0994b5a23d3b968847ed4d202111a1fe86680a63
Jansen-Stanlie <jansenstan24@gmail.com>
2021-08-08 23:27:15 +0700

    update axios localserver

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -10,6 +10,20 @@ class BlogPost extends Component {
 			post: [],
 		};
 	}
+	getPostAPI = () => {
+		axios.get("http://localhost:3000/posts").then((res) => {
+			console.log(res);
+			this.setState({
+				post: res.data,
+			});
+		});
+	};
+	handleRemove = (data) => {
+		axios.delete(`http://localhost:3000/posts/${data}`).then((res) => {
+			console.log(res);
+			this.getPostAPI();
+		});
+	};
 	componentDidMount() {
 		// fetch("https://jsonplaceholder.typicode.com/posts")
 		// 	.then((response) => response.json())
@@ -18,19 +32,28 @@ class BlogPost extends Component {
 		// 			post: json,
 		// 		});
 		// 	});
-		axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-			console.log(res);
-			this.setState({
-				post: res.data,
-			});
-		});
+		// axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+		// 	console.log(res);
+		// 	this.setState({
+		// 		post: res.data,
+		// 	});
+		// });
+		this.getPostAPI();
 	}
 	render() {
 		return (
 			<Fragment>
 				<p className="section-title">Blog Post</p>
 				{this.state.post.map((post) => {
-					return <Post key={post.id} title={post.title} desc={post.body} />;
+					return (
+						<Post
+							key={post.id}
+							title={post.title}
+							desc={post.body}
+							data={post}
+							remove={this.handleRemove}
+						/>
+					);
 				})}
 			</Fragment>
 		);
